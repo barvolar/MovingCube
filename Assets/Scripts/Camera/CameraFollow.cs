dc0ef3d99dec6552753817cf4bd4568a9226321e
barvolar <kostam96@yandex.ru>
2025-09-17 20:39:14 +0300

    настройка камеры 3

--- Assets/Scripts/Camera/CameraFollow.cs
+++ Assets/Scripts/Camera/CameraFollow.cs
@@ -3,10 +3,12 @@
 public class CameraFollow : MonoBehaviour
 {
     [SerializeField] private Transform target;        // Игрок
-    [SerializeField] private float followSpeed = 5f;  // Плавность движения Pivot
-    [SerializeField] private float rotationSpeed = 3f; // Чувствительность мыши
+    [SerializeField] private Camera cam;              // Вложенная камера
+    [SerializeField] private float followSmooth = 0.1f;   // сглаживание позиции
     [SerializeField] private float rotationSmooth = 0.1f; // сглаживание вращения
+    [SerializeField] private float rotationSpeed = 3f;
 
+    private Vector3 velocity = Vector3.zero;
     private float yaw = 0f;
     private float pitch = 20f;
 
@@ -14,17 +16,16 @@ public class CameraFollow : MonoBehaviour
     {
         if (target == null) return;
 
-        // --- Вращение мышью ---
+        // --- Плавное вращение Pivot мышью ---
         yaw += Input.GetAxis("Mouse X") * rotationSpeed;
         pitch -= Input.GetAxis("Mouse Y") * rotationSpeed;
         pitch = Mathf.Clamp(pitch, 5f, 80f);
 
-        // --- Плавное движение Pivot к игроку ---
-        Vector3 desiredPosition = target.position;
-        transform.position = Vector3.Lerp(transform.position, desiredPosition, followSpeed * Time.deltaTime);
-
-        // --- Плавный поворот Pivot ---
         Quaternion targetRotation = Quaternion.Euler(pitch, yaw, 0f);
         transform.rotation = Quaternion.Slerp(transform.rotation, targetRotation, rotationSmooth);
+
+        // --- Плавное движение Pivot за кубом ---
+        Vector3 desiredPosition = target.position;
+        transform.position = Vector3.SmoothDamp(transform.position, desiredPosition, ref velocity, followSmooth);
     }
 }
