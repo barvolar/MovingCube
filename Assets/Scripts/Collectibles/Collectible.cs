b1e16e5b509f2478d31cc683d57d4c7abc752ce3
barvolar <kostam96@yandex.ru>
2025-09-17 19:28:28 +0300

    добавил логику спавна эффекта

--- Assets/Scripts/Collectibles/Collectible.cs
+++ Assets/Scripts/Collectibles/Collectible.cs
@@ -2,12 +2,16 @@ using UnityEngine;
 
 public class Collectible : MonoBehaviour
 {
+    [SerializeField] private ParticleSystem _collectibleEffectPrefab;
     [SerializeField] private int _valuePoint;
 
     private void OnTriggerEnter(Collider other)
     {
         if (other.CompareTag("Player"))
         {
+            if (_collectibleEffectPrefab != null)
+                Instantiate(_collectibleEffectPrefab, transform.position, Quaternion.identity);
+
             ScoreManager.Instance.AddScore(_valuePoint);
             Destroy(gameObject);
         }
